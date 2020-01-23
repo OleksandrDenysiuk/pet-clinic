32a1c49eaa55f371d9a9814cd9e08a52b93aedd6
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-01-23 12:08:38 +0100

    List all Owners on Owner page. Closes #16

--- pet-clinic-web/src/main/java/denysiuk/springprojects/petclinic/controller/OwnersController.java
+++ pet-clinic-web/src/main/java/denysiuk/springprojects/petclinic/controller/OwnersController.java
@@ -1,12 +1,23 @@
 package denysiuk.springprojects.petclinic.controller;
 
+import denysiuk.springprojects.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class OwnersController {
+
+    private final OwnerService ownerService;
+
+    public OwnersController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+
     @GetMapping("/owners")
-    public String listOwners(){
+    public String listOwners(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
