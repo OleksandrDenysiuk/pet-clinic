c36bfbfa8956b8f20ab80c1cca0b0f7a3ee53c52
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-01-30 09:45:16 +0100

    Added visits, updated owner with pet relationship. Closes #25

--- pet-clinic-data/src/main/java/denysiuk/springprojects/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/denysiuk/springprojects/petclinic/model/Owner.java
@@ -1,4 +1,16 @@
 package denysiuk.springprojects.petclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+
+    private Set<Pet> Pets;
+
+    public Set<Pet> getPets() {
+        return Pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        Pets = pets;
+    }
 }
