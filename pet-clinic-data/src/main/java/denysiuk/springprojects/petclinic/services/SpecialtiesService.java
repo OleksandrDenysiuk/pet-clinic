b56f6984b083c8fd79d297ece29bf7aa9acc5854
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-01-30 15:00:01 +0100

    Added Specialties Map Impl. Closes #30

---
+++ pet-clinic-data/src/main/java/denysiuk/springprojects/petclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package denysiuk.springprojects.petclinic.services;
+
+import denysiuk.springprojects.petclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/denysiuk/springprojects/petclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,33 @@
+package denysiuk.springprojects.petclinic.services.map;
+
+import denysiuk.springprojects.petclinic.model.Speciality;
+import denysiuk.springprojects.petclinic.services.SpecialtiesService;
+
+import java.util.Set;
+
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialtiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
