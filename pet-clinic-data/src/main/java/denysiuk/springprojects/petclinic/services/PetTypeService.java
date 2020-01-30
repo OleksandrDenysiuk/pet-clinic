68d6bf373baabae12d4f5cc424f83b79030894a9
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-01-30 10:05:29 +0100

    Created PetType Map Service Impl. Closes #28

---
+++ pet-clinic-data/src/main/java/denysiuk/springprojects/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package denysiuk.springprojects.petclinic.services;
+
+import denysiuk.springprojects.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
