846dd68891dcf28e9760969f76f535cfbf0fb539
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-02-08 16:14:22 +0200

    Implemented Vets JSON Endpoint. #60

--- pet-clinic-web/src/main/java/denysiuk/springprojects/petclinic/controller/VetsController.java
+++ pet-clinic-web/src/main/java/denysiuk/springprojects/petclinic/controller/VetsController.java
@@ -1,9 +1,13 @@
 package denysiuk.springprojects.petclinic.controller;
 
+import denysiuk.springprojects.petclinic.model.Vet;
 import denysiuk.springprojects.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetsController {
@@ -19,5 +23,10 @@ public class VetsController {
 
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getJsonFromatVets(){
+        return vetService.findAll();
     }
 }
